5236f822d0a86650ad7ed5ccff82e76174be377f
Mathieu-COSYNS <64072917+Mathieu-COSYNS@users.noreply.github.com>
2025-01-25 15:23:39 +0100

    refactor: remove unused TerminalFormatter

--- music_sync/core/logging.py
+++ music_sync/core/logging.py
@@ -10,33 +10,9 @@ LOG_LEVELS = {
 }
 
 
-class TerminalFormatter(logging.Formatter):
-    grey = "\x1b[38;20m"
-    yellow = "\x1b[33;20m"
-    red = "\x1b[31;20m"
-    bold_red = "\x1b[31;1m"
-    reset = "\x1b[0m"
-    format = (
-        "%(asctime)s - %(name)s - %(levelname)s - %(message)s (%(filename)s:%(lineno)d)"
-    )
-
-    FORMATS = {
-        logging.DEBUG: grey + format + reset,
-        logging.INFO: grey + format + reset,
-        logging.WARNING: yellow + format + reset,
-        logging.ERROR: red + format + reset,
-        logging.CRITICAL: bold_red + format + reset,
-    }
-
-    def format(self, record):
-        log_fmt = self.FORMATS.get(record.levelno)
-        formatter = logging.Formatter(log_fmt)
-        return formatter.format(record)
-
-
 def setup_logging(level: str | None = None):
-    level_clean = level.upper()
-    level_clean = LOG_LEVELS.get(level_clean, level_clean)
+    level_clean = level.upper() if level is not None else "I"
+    level_clean = LOG_LEVELS.get(level_clean, "INFO")
 
     if not isinstance(getattr(logging, level_clean, None), int):
         raise ValueError(f"Invalid log level: {level}")
